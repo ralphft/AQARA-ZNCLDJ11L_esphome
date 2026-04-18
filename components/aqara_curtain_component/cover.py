0993d8131a55c4b7e832ebfa6d0a1aac8d38e08b
Ralph <ski@thor.edu>
2026-04-18 11:21:52 +0200

    Try

--- components/aqara_curtain_component/cover.py
+++ components/aqara_curtain_component/cover.py
@@ -1,28 +1,3 @@
-import esphome.codegen as cg
-import esphome.config_validation as cv
-from esphome.components import cover
-from esphome.const import CONF_ID
-from . import AqaraCurtainComponent, aqara_curtain_ns
-
-DEPENDENCIES = ["aqara_curtain_component"]
-
-AqaraCurtainCover = aqara_curtain_ns.class_("AqaraCurtainCover", cover.Cover)
-
-CONF_AQARA_CURTAIN_ID = "aqara_curtain_id"
-
-CONFIG_SCHEMA = cover.COVER_SCHEMA.extend(
-    {
-        cv.GenerateID(): cv.declare_id(AqaraCurtainCover),
-        cv.Required(CONF_AQARA_CURTAIN_ID): cv.use_id(AqaraCurtainComponent),
-    }
-)
-
-
-async def to_code(config):
-    var = cg.new_Pvariable(config[CONF_ID])
-    await cover.register_cover(var, config)
-
-    parent = await cg.get_variable(config[CONF_AQARA_CURTAIN_ID])
-    cg.add(parent.set_cover(var))
-
-
+# Compatibility shim for ESPHome versions that still load <component>/cover.py.
+# Re-export the package-based implementation at <component>/cover/__init__.py.
+from .cover import *  # noqa: F401,F403
---
+++ components/aqara_curtain_component/cover/__init__.py
@@ -0,0 +1,28 @@
+import esphome.codegen as cg
+import esphome.config_validation as cv
+from esphome.components import cover
+from esphome.const import CONF_ID
+
+from .. import AqaraCurtainComponent, aqara_curtain_ns
+
+DEPENDENCIES = ["aqara_curtain_component"]
+
+AqaraCurtainCover = aqara_curtain_ns.class_("AqaraCurtainCover", cover.Cover)
+
+CONF_AQARA_CURTAIN_ID = "aqara_curtain_id"
+
+CONFIG_SCHEMA = cover.COVER_SCHEMA.extend(
+    {
+        cv.GenerateID(): cv.declare_id(AqaraCurtainCover),
+        cv.Required(CONF_AQARA_CURTAIN_ID): cv.use_id(AqaraCurtainComponent),
+    }
+)
+
+
+async def to_code(config):
+    var = cg.new_Pvariable(config[CONF_ID])
+    await cover.register_cover(var, config)
+
+    parent = await cg.get_variable(config[CONF_AQARA_CURTAIN_ID])
+    cg.add(parent.set_cover(var))
+
