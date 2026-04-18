d757317465ab1cf69bf031a528f87295af2cdc5f
Ralph <ski@thor.edu>
2026-04-18 11:04:23 +0200

    Fix wrong file

--- components/ZNCLDJ11L_component/cover.py
+++ components/ZNCLDJ11L_component/cover.py
@@ -1,31 +1,28 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
-from esphome.components import uart
+from esphome.components import cover
 from esphome.const import CONF_ID
+from . import AqaraCurtainComponent, aqara_curtain_ns
 
-DEPENDENCIES = ["uart"]
-AUTO_LOAD = ["cover"]
+DEPENDENCIES = ["aqara_curtain"]
 
-CODEOWNERS = ["@ralphft"]
+AqaraCurtainCover = aqara_curtain_ns.class_("AqaraCurtainCover", cover.Cover)
 
-aqara_curtain_ns = cg.esphome_ns.namespace("aqara_curtain")
-AqaraCurtainComponent = aqara_curtain_ns.class_(
-    "AqaraCurtainComponent", cg.Component, uart.UARTDevice
-)
+CONF_AQARA_CURTAIN_ID = "aqara_curtain_id"
 
-CONFIG_SCHEMA = (
-    cv.Schema(
-        {
-            cv.GenerateID(): cv.declare_id(AqaraCurtainComponent),
-        }
-    )
-    .extend(uart.UART_DEVICE_SCHEMA)
-    .extend(cv.COMPONENT_SCHEMA)
+CONFIG_SCHEMA = cover.COVER_SCHEMA.extend(
+    {
+        cv.GenerateID(): cv.declare_id(AqaraCurtainCover),
+        cv.Required(CONF_AQARA_CURTAIN_ID): cv.use_id(AqaraCurtainComponent),
+    }
 )
 
 
 async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
-    await cg.register_component(var, config)
-    await uart.register_uart_device(var, config)
+    await cover.register_cover(var, config)
+
+    parent = await cg.get_variable(config[CONF_AQARA_CURTAIN_ID])
+    cg.add(parent.set_cover(var))
+
 
